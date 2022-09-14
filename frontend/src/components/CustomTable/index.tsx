d1e911222bcaf9d5574aa93d3c81965027d82766
LucasBerce71 <lucasberce71@gmail.com>
2022-09-14 01:03:39 -0300

    feat

--- frontend/src/components/CustomTable/index.tsx
+++ frontend/src/components/CustomTable/index.tsx
@@ -1,39 +1,53 @@
-import React, { memo } from 'react';
-
-import MaterialTable from '@material-table/core';
-import { ExportCsv, ExportPdf } from '@material-table/exporters';
+import React, { memo } from "react";
+import { MuiThemeProvider } from "@material-ui/core";
+import { ExportCsv, ExportPdf } from "@material-table/exporters";
+import MaterialTable from "@material-table/core";
 
 import { ICustomTableProps } from '../../models/CustomTable';
-import Checkbox from '../Checkbox';
+
+import { customMUITheme } from "../../theme/customMUITheme";
 
 const CustomTable: React.FC<ICustomTableProps> = ({
   title,
   columns,
   data,
-  options
+  options,
 }) => {
   return (
-    <MaterialTable
-      title={title}
-      columns={columns}
-      data={data}
-      options={options?.exportData ? {
-        exportMenu: [
-          {
-            label: "Exportar para PDF",
-            exportFunc: (cols, datas) => 
-              ExportPdf(cols, datas, options?.exportFileName || ""),
-          },
-          {
-            label: "Exportar para CSV",
-            exportFunc: (cols, datas) => 
-              ExportCsv(cols, datas, options?.exportFileName || ""),
-          },
-        ],
-  
-      }: {}}
-      style={{ margin: "30px 90px" }}
-    />
+    <MuiThemeProvider theme={customMUITheme}>
+      <MaterialTable
+        title={title}
+        columns={columns}
+        data={data}
+        localization={{
+          toolbar: {
+            nRowsSelected: "{0} linha(s) selecionada(s)"
+          }
+        }}
+        options={
+          options?.exportData
+            ? {
+                exportMenu: [
+                  {
+                    label: "Exportar para PDF",
+                    exportFunc: (cols, datas) =>
+                      ExportPdf(cols, datas, options?.exportFileName || ""),
+                  },
+                  {
+                    label: "Exportar para CSV",
+                    exportFunc: (cols, datas) =>
+                      ExportCsv(cols, datas, options?.exportFileName || ""),
+                  },
+                ],
+                selection: true,
+              }
+            : {
+                selection: true,
+              }
+        }
+        style={{ margin: "30px 90px" }}
+      />
+    </MuiThemeProvider>
   );
 };
 
